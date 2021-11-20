83807073135d616b94155d750bac814354b404fb
Filip Durca <filip.durca@gmail.com>
2021-11-20 18:10:42 -0500

    Working search and definition matcher

--- content.js
+++ content.js
@@ -1,14 +1,15 @@
 // Get location of all instances of term
-var result = document.evaluate(("//text()[contains(., 'inflation')]"), document, null, XPathResult.ANY_TYPE, null);
-var node, nodes = [];
-
-// Add all nodes to node array
-while (node = result.iterateNext()){
-    if (!(node.textContent.includes("{") || node.textContent.includes("}"))){
-        nodes.push(node);
-    }
-}
-
+// var result = document.evaluate(("//text()[contains(translate(., 'ABCDEFGHIJKLMNOPQRSTUVWXYZ', 'abcdefghijklmnopqrstuvwxyz'), 'inflation')]"), document, null, XPathResult.ANY_TYPE, null);
+// var node, nodes = [];
+//
+// // Add all nodes to node array
+// while (node = result.iterateNext()){
+//     if (!(node.textContent.includes("{") || node.textContent.includes("}"))){
+//         nodes.push(node);
+//         console.log(node);
+//     }
+// }
+//
 // Add in line html
 // setTimeout(function() {
 //     for (const n of nodes){
@@ -21,5 +22,27 @@ while (node = result.iterateNext()){
 //         console.log(n)
 //     }
 //   }, 5000);
+
+// url = chrome.runtime.getURL('terms.json');
+// counter = 0;
+// found = []
+
+// fetch(url)
+// .then((response) => response.json()) // file contains json
+// .then((json) => {
+//     setTimeout(function() {
+        
+//         for (var i = 0; i < json.length; i++){
+//             var obj = json[i];
+//             if ((document.documentElement.textContent || document.documentElement.innerText)
+//             .indexOf(obj.term) > -1) {
+//                 console.log(obj.term + ": " + obj.description)
+//                 found[counter];
+//                 counter++;
+//             }
+//         }
+//         console.log(counter);
+//     }, 5000);
+// }); 
 
 
--- popup.js
+++ popup.js
@@ -1,1 +1,21 @@
+url = chrome.runtime.getURL('terms.json');
+counter = 0;
+found = []
 
+fetch(url)
+.then((response) => response.json()) // file contains json
+.then((json) => {
+    setTimeout(function() {
+        
+        for (var i = 0; i < json.length; i++){
+            var obj = json[i];
+            if ((document.documentElement.textContent || document.documentElement.innerText)
+            .indexOf(obj.term) > -1) {
+                document.write(obj.term + ": " + obj.description)
+                found[counter];
+                counter++;
+            }
+        }
+        console.log(counter);
+    }, 5000);
+}); 
