56585da3eb0f18f013f6acc7eea6e1d1099a82c0
Filip Durca <filip.durca@gmail.com>
2021-11-20 23:23:32 -0500

    Fully functional extension, needs styling

--- content.js
+++ content.js
@@ -1,31 +1,33 @@
 // Get location of all instances of term
-// var result = document.evaluate(("//text()[contains(translate(., 'ABCDEFGHIJKLMNOPQRSTUVWXYZ', 'abcdefghijklmnopqrstuvwxyz'), 'inflation')]"), document, null, XPathResult.ANY_TYPE, null);
-// var node, nodes = [];
-//
-// // Add all nodes to node array
-// while (node = result.iterateNext()){
-//     if (!(node.textContent.includes("{") || node.textContent.includes("}"))){
-//         nodes.push(node);
-//         console.log(node);
-//     }
-// }
-//
+var result = document.evaluate(("//text()[contains(translate(., 'ABCDEFGHIJKLMNOPQRSTUVWXYZ', 'abcdefghijklmnopqrstuvwxyz'), 'inflation')]"), document, null, XPathResult.ANY_TYPE, null);
+var node, nodes = [];
+
+// Add all nodes to node array
+while (node = result.iterateNext()){
+    if (!(node.textContent.includes("{") || node.textContent.includes("}"))){
+        nodes.push(node);
+    }
+}
+
+console.log("HIGHLIGHTER")
 // Add in line html
-// setTimeout(function() {
-//     for (const n of nodes){
-//         var s = n.innerHTML;
-//         var parentNode = n.parentNode;
-//         var s1 = s.replace("inflation", '<span style="background-color: #FF0000">inflation</span>');
-//         var tempNode = document.createTextNode(s1);
-//         tempNode.innerHTML = s1
-//         parentNode.replaceChild(tempNode, n)
-//         console.log(n)
-//     }
-//   }, 5000);
+setTimeout(function() {
+    for (const n of nodes){
+        var s = n.textContent;
+        var parentNode = n.parentNode;
+        var s1 = s.replace("inflation", '<span style="background-color: #FF0000">inflation</span>');
+        var tempNode = document.createTextNode(s1);
+        n.innerHTML = s1
+        console.log(n)
+    }
+  }, 5000);
+
+// OY
 
 // url = chrome.runtime.getURL('terms.json');
 // counter = 0;
 // found = []
+// console.log(document);
 
 // fetch(url)
 // .then((response) => response.json()) // file contains json
--- popup.js
+++ popup.js
@@ -1,21 +1,58 @@
-url = chrome.runtime.getURL('terms.json');
-counter = 0;
-found = []
+// Run function on page load
+document.getElementById("title").addEventListener("DOMContentLoaded", myFunction());
 
-fetch(url)
-.then((response) => response.json()) // file contains json
-.then((json) => {
-    setTimeout(function() {
-        
-        for (var i = 0; i < json.length; i++){
-            var obj = json[i];
-            if ((document.documentElement.textContent || document.documentElement.innerText)
-            .indexOf(obj.term) > -1) {
-                document.write(obj.term + ": " + obj.description)
-                found[counter];
-                counter++;
-            }
-        }
-        console.log(counter);
-    }, 5000);
-}); 
+// Function
+function myFunction(){
+
+    // Variables
+    // Get terms from local json
+    url = chrome.runtime.getURL('terms.json');
+    // Total words found
+    counter = 0;
+
+    // Get url from domain
+    let queryOptions = { active: true, currentWindow: true };
+    let tab = chrome.tabs.query(queryOptions);
+    let domURL;
+
+    // Dont worry about this
+    Promise.resolve(tab).then((result) => {
+        domURL = (result[0]["url"])
+        fetch(domURL)
+        .then((site) => {return site.text();})
+        .then(function(string) {
+            fetch(url)
+            .then((response) => response.json())
+            .then((json) => {
+                setTimeout(function() {
+
+                    // For loop to add all terms + definitions to extension pop up
+                    for (var i = 0; i < json.length; i++){
+
+                        // Object has 2 values, term and definition
+                        var obj = json[i];
+
+                        // If string contains term
+                        if ((string).indexOf(obj.term) > -1) {
+                            counter++;
+                            // STYLING TO DO HERE
+                            var s = obj.term + ": " + obj.description + "\n\n"
+                            var tempDiv = document.createElement("div");
+                            var tempNode = document.createTextNode(s);
+                            tempDiv.appendChild(tempNode);
+                            document.body.append(tempDiv);
+
+                        }
+                    }
+                    // SOME STYLING HERE IF NONE FOUND
+                    if (counter == 0)
+                        document.getElementById("title").textContent = "Nothing Found!";
+                    else
+                        document.getElementById("title").textContent = "Words Found:";
+                }, 3000);
+            });
+        })
+    });
+}
+
+
